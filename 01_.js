17e9a5cedcfba967eeba8716438b3222ee030541
Dev-Arun7 <123arunblakrishnan@gmail.com>
2024-05-13 11:21:47 +0530

    First commit

---
+++ 01_.js
@@ -0,0 +1,19 @@
+
+- Collection: posts
+  - Document 1:
+    {
+      "_id": ObjectId("60a6cf87499f11761ce3ea7b"),
+      "title": "Introduction to MongoDB",
+      "content": "MongoDB is a NoSQL database...",
+      "author": "Arun",
+      "tags": ["mongodb", "database", "nosql"]
+    }
+  - Document 2:
+    {
+      "_id": ObjectId("60a6d018499f11761ce3ea7c"),
+      "title": "Getting Started with Python",
+      "content": "Python is a popular programming language...",
+      "author": "Emily",
+      "tags": ["python", "programming", "beginner"]
+    }
+  ...
